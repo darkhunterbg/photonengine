43e6403bdea0619e49053701e62e10498a3e4e14
Kaloyan Nachev <kaloyan.r.nachev@gmail.com>
2016-05-30 16:05:24 +0300

    Fixing build configurations

--- Game/src/Main.cpp
+++ Game/src/Main.cpp
@@ -1,6 +1,15 @@
 #include <Windows.h>
 
+constexpr int GetValue()
+{
+	return 0;
+}
+
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
+	int x = GetValue();
+
+	++x;
+
 	return 0;
 }
