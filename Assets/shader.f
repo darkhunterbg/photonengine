a177f32f5756cfee1a7f9fff2d0f0add88964b6a
Kaloyan Nachev <kaloyan.r.nachev@gmail.com>
2016-06-07 10:35:10 +0300

    Shader testing

--- Assets/shader.f
+++ Assets/shader.f
@@ -1,8 +1,17 @@
 #version 330 core
+#extension GL_ARB_explicit_uniform_location : enable
+#extension GL_ARB_explicit_attrib_location : enable
+#extension GL_ARB_enhanced_layouts : enable
+#extension GL_ARB_separate_shader_objects : enable
 
-out vec4 output;
+//outputs to frame buffer 0
+layout(location = 0) out vec4 output;
 
-uniform vec4 color;
+//structure, aligned by vec4, starts at location 0
+layout( location = 0, std140 ) uniform Block
+{
+ vec4 color;
+};
 
 void main()
 {
